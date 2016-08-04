547971cfdfcb54e1b134bf5670710e7872631c3e
Sean <Fancysaurus@gmail.com>
2016-08-04 14:57:08 -0400

    -Removed Example mod -Created Lib Misc

---
+++ src/main/java/tas/dfa/common/DFArtifacts.java
@@ -0,0 +1,15 @@
+package tas.dfa.common;
+
+import net.minecraftforge.fml.common.Mod;
+import tas.dfa.common.lib.LibMisc;
+
+/**
+ * Created by fancysaurus on 8/4/16.
+ */
+
+@Mod(modid = LibMisc.MOD_ID, name = LibMisc.MOD_NAME, version = LibMisc.VERSION)
+public class DFArtifacts
+{
+    @Mod.Instance(LibMisc.MOD_ID)
+    public static DFArtifacts instance;
+}
---
+++ src/main/java/tas/dfa/common/lib/LibMisc.java
@@ -0,0 +1,13 @@
+package tas.dfa.common.lib;
+
+/**
+ * Created by fancysaurus on 8/4/16.
+ */
+public class LibMisc
+{
+    public static final String MOD_ID = "dfa";
+    public static final String MOD_NAME = "DFArtifacts";
+    public static final String BUILD = "GRADLE:BUILD";
+    public static final String VERSION = "GRADLE:VERSION-" + BUILD;
+
+}
