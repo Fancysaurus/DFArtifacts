75981a3f3fb027b3b2173f54a18a11f604b753a4
Sean <Fancysaurus@gmail.com>
2016-08-06 03:34:54 -0400

    Fixed world showing through anvil

--- src/main/java/tas/dfa/common/block/BlockAncientAnvil.java
+++ src/main/java/tas/dfa/common/block/BlockAncientAnvil.java
@@ -2,6 +2,7 @@ package tas.dfa.common.block;
 
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumBlockRenderType;
 import tas.dfa.common.block.base.BaseBlock;
 import tas.dfa.common.lib.LibBlocks;
@@ -15,5 +16,18 @@ public class BlockAncientAnvil extends BaseBlock {
         super(LibBlocks.ancientAnvil,Material.ANVIL);
     }
 
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
 
+    @Override
+    public boolean isFullCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public BlockRenderLayer getBlockLayer() {
+        return BlockRenderLayer.CUTOUT;
+    }
 }
