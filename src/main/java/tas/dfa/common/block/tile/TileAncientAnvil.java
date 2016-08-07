3fe0c8720acf4e44facf7ecb47b5b9f2f52cfffa
David <aravol.amakiir@gmail.com>
2016-08-06 22:28:41 -0600

    Implemented draw code

--- src/main/java/tas/dfa/common/block/tile/TileAncientAnvil.java
+++ src/main/java/tas/dfa/common/block/tile/TileAncientAnvil.java
@@ -3,7 +3,9 @@ package tas.dfa.common.block.tile;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagByte;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagInt;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.AxisAlignedBB;
 import tas.dfa.Api.Config;
@@ -16,6 +18,9 @@ import java.util.Random;
  * Created by David on 8/4/2016.
  */
 public class TileAncientAnvil extends BaseTile implements ITickable {
+
+    private int currentDrawValue = 0;
+    private boolean isWaitingForBaseItem = true;
 
     @Override
     public void update() {
@@ -31,11 +36,11 @@ public class TileAncientAnvil extends BaseTile implements ITickable {
         for(EntityItem item : items) {
             ItemStack stack = item.getEntityItem();
 
-            if(getIsWaitingForBaseItems()){
+            if(isWaitingForBaseItem){
                 if(!Config.instance.isValidBaseItem(stack))
                     continue;
                 consumeItem(item);
-                setIsWaitingForBaseItems(false);
+                isWaitingForBaseItem = false;
             }
             else {
                 if(!Config.instance.isValidDrawItem(stack))
@@ -48,17 +53,34 @@ public class TileAncientAnvil extends BaseTile implements ITickable {
 
     @Override
     public void readCustomNBT(NBTTagCompound tag) {
-        // TODO: ?
+            isWaitingForBaseItem = tag.hasKey("isWaitingForBaseItem")
+                    ? tag.getBoolean("isWaitingForBaseItem")
+                    : true;
+
+            currentDrawValue = tag.hasKey("currentDrawValue")
+                    ? tag.getInteger("currentDrawValue")
+                    : 0;
     }
 
     @Override
     public void writeCustomNBT(NBTTagCompound tag) {
-        // TODO: ?
+        tag.setBoolean("isWaitingForBaseItem", isWaitingForBaseItem);
+        tag.setInteger("currentDrawValue", currentDrawValue);
     }
 
-    public boolean getIsWaitingForBaseItems() {
-        // TODO: Check NBT data
-        return false;
+    public void activate(EntityPlayer playerIn) {
+        if(currentDrawValue > 21)
+            ; // TODO: Punish the greedy player!
+        else
+            ; // TODO: Reward player with an artifact!
+
+        reset();
+    }
+
+    public void reset() {
+        isWaitingForBaseItem = false;
+        currentDrawValue = 0;
+        markDirty();
     }
 
     private void consumeItem(EntityItem entity) {
@@ -70,18 +92,12 @@ public class TileAncientAnvil extends BaseTile implements ITickable {
             entity.setEntityItemStack(stack);
     }
 
-    private void setIsWaitingForBaseItems(boolean value) {
-        // TODO: Set NBT data
-    }
-
     private void doDraw() {
         Random rng = new Random();
 
-        // TODO: Add random value to score
-        // TODO: Check for overflow of score
-    }
+        int value = rng.nextInt(13) + 1;
+        if(value > 10) value = 10;
 
-    public void activate(EntityPlayer playerIn) {
-        // TODO: Check current data and either punish the player or give them an artifact
+        currentDrawValue += value;
     }
 }
